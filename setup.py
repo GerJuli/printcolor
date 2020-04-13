07029c9ba401d8ec850fd499d924e90cdc529c86
Julian-Samuel Gebühr <Julian-Samuel Gebhr>
2020-04-13 23:09:38 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
+    name='printcolor',
+    version='0.0.1',
+    description='Easy printing in color',
+    long_description='Print text to the stdout in defined colors',
+    author='Julian-Samuel Gebuehr',
+    author_email='julian-samuel@gebuehr.net',
+    url='https://github.com/GerJuli/printcolor.git',
+    download_url='https://github.com/GerJuli/printcolor.git',
+    license='MIT',
+    packages=['printcolor'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Children',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3'
+        ]
+)
